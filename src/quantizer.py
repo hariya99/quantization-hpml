6d11427cff53ae184c508f694778d25ff7db46ec
Harish Chauhan <harish.chauhan99@gmail.com>
2022-05-15 03:25:57 +0000

    1. Restructure code to avoid code repetition. 2. fix bug where original model was being run in quantized loop. 3. Generate memory statistics

--- src/quantizer.py
+++ src/quantizer.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
+import os 
 import torch
 import torch.nn as nn
+import copy 
 
 QTensor = namedtuple('QTensor', ['tensor', 'scale', 'zero_point'])
 
@@ -74,48 +76,78 @@ def dequantize_tensor(q_x):
 #         return grad_output, None, None, None
 
 
+def model_iterator(model: nn.Module):
+    """
+    Traverse through the model and yields components of a model. 
+    """
+    # Created this to avoid code repetition.
+    
+    for layer in model.children():
+        layer_name = layer._get_name()
+        for key in layer._parameters.keys():
+            name = layer_name + '.' + key
+            yield layer, name, key 
 
 
-
-def simulate_quantization(net: nn.Module, stats: dict):
+def simulate_quantization(model: nn.Module, stats: dict):
     
     """
-        This function tranverses through the layers of a network, 
-        stores its orignal weights for backward pass and 
-        quantizes-dequantizes its weights. This also populate a running stats 
+        Traverse through the layers of a network, 
+        store its orignal weights for backward pass and 
+        quantize-dequantize its weights. Populate a running stats 
         dictionary which behaves like a pytorch observer class.  
     """
 
     original_weights = {}
+    
+    for layer, name, key in model_iterator(model):
+        
+        # hold onto original weights 
+        weights = layer._parameters[key].data
+        original_weights[name] = weights
+        # receives a named tuple 
+        weights  = quantize_tensor(weights)
+        stats[name] = tuple((weights.scale, weights.zero_point))
 
-    for layer in net.children():
-        layer_name = layer._get_name()
-        for key in layer._parameters.keys():
-            name = layer_name + '.' + key
-            # hold onto original weights 
-            weights = layer._parameters[key].data
-            original_weights[name] = weights
-            # receives a named tuple 
-            weights  = quantize_tensor(weights)
-            stats[name] = tuple((weights.scale, weights.zero_point))
+        # dequantization happens on quantized weights 
+        # they have some precision loss from original weights 
+        weights = dequantize_tensor(weights)
 
-            # dequantization happens on quantized weights 
-            # they have some precision loss from original weights 
-            weights = dequantize_tensor(weights)
-
-            # replace weights with dequantized weights
-            layer._parameters[key].data = weights
+        # replace weights with dequantized weights
+        layer._parameters[key].data = weights
     
     return original_weights
 
 
-def restore_original_weights(net: nn.Module, weights: dict):
+def restore_original_weights(model: nn.Module, weights: dict):
     """
     Restore the original weights of the network for the backward 
     pass.
     """
-    for layer in net.children():
-        layer_name = layer._get_name()
-        for key in layer._parameters.keys():
-            name = layer_name + '.' + key
-            layer._parameters[key].data = weights[name] 
+    for layer, name, key in model_iterator(model):
+        layer._parameters[key].data = weights[name] 
+
+
+    
+def calculate_size(model: nn.Module, quantized: bool = False):
+    """
+    Calculate the model size in MB. Turn on the flag if you want to 
+    calculate the size of a quantized model.
+    """
+    model_cp = copy.deepcopy(model) 
+
+    if quantized:
+        print("quantized")
+        for layer, name, key in model_iterator(model_cp):
+            weights = layer._parameters[key].data
+            weights  = quantize_tensor(weights)
+            layer._parameters[key].data = weights.tensor
+    
+    torch.save(model_cp.state_dict(), "temp.p")
+    size = f'Size (MB): {os.path.getsize("temp.p")/1e6}'
+    os.remove('temp.p')
+    return size
+                
+        
+        
+           
